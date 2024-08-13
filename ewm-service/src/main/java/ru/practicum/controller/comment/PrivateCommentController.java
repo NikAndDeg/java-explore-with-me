0360fd95845f09f011a6bbb334e075d4e18e89bf
Nikolai Degtiarev <NikAndDeg@yandex.ru>
2024-08-13 16:14:04 +0500

    Реализовал функциональность комментариев

---
+++ ewm-service/src/main/java/ru/practicum/controller/comment/PrivateCommentController.java
@@ -0,0 +1,77 @@
+package ru.practicum.controller.comment;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.model.dto.comment.CommentDto;
+import ru.practicum.model.dto.comment.DeletedCommentDto;
+import ru.practicum.model.dto.comment.NewCommentDto;
+import ru.practicum.service.comment.CommentService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Validated
+@RestController
+@RequestMapping(path = "/users/{userId}/comments")
+public class PrivateCommentController {
+	private final CommentService commentService;
+	private static final String dateTimePattern = "yyyy-MM-dd HH:mm:ss";
+
+	//POST /users/{userId}/comments/{eventId}
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping("/{eventId}")
+	public CommentDto addComment(@PathVariable @Min(1) int userId,
+								 @PathVariable @Min(1) int eventId,
+								 @RequestBody @Valid NewCommentDto newCommentDto) {
+		log.info("Request to add comment with userId[{}], eventId[{}], comment[{}].",
+				userId, eventId, newCommentDto);
+		CommentDto addedComment = commentService.addComment(userId, eventId, newCommentDto);
+		log.info("Comment[{}] added.", addedComment);
+		return addedComment;
+	}
+
+	//DELETE /users/{userId}/comments/{commentId}
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{commentId}")
+	public DeletedCommentDto deleteComment(@PathVariable @Min(1) int userId,
+										   @PathVariable @Min(1) int commentId) {
+		log.info("Request to delete comment with userId[{}], commentId[{}].", userId, commentId);
+		DeletedCommentDto deletedComment = commentService.deleteCommentByUser(userId, commentId);
+		log.info("Comment[{}] deleted.", deletedComment);
+		return deletedComment;
+	}
+
+	//GET /users/{userId}/comments/{commentId}
+	@GetMapping("/{commentId}")
+	public CommentDto getComment(@PathVariable @Min(1) int userId,
+								 @PathVariable @Min(1) int commentId) {
+		log.info("Request to get comment with userId[{}], commentId[{}].", userId, commentId);
+		CommentDto commentDto = commentService.getCommentByUser(userId, commentId);
+		log.info("Comment[{}] received.", commentDto);
+		return commentDto;
+	}
+
+	//GET /users/{userId}/comments?eventId=int&from=int&size=int
+	@GetMapping
+	public List<CommentDto> getComments(@PathVariable @Min(1) int userId,
+										@RequestParam @Min(1) int eventId,
+										@RequestParam(required = false) String text,
+										@RequestParam(required = false) @DateTimeFormat(pattern = dateTimePattern) LocalDateTime rangeStart,
+										@RequestParam(required = false) @DateTimeFormat(pattern = dateTimePattern) LocalDateTime rangeEnd,
+										@RequestParam(defaultValue = "0") @Min(0) int from,
+										@RequestParam(defaultValue = "10") @Min(1) int size) {
+		log.info("Request to get comments with userId[{}], eventId[{}], text[{}], start[{}], end[{}], from[{}], size[{}].",
+				userId, eventId, text, rangeStart, rangeEnd, from, size);
+		List<CommentDto> comments = commentService.getCommentsByUser(userId, eventId, text, rangeStart, rangeEnd, from, size);
+		log.info("Comments received.");
+		return comments;
+	}
+}
---
+++ ewm-service/src/main/java/ru/practicum/service/comment/CommentServiceImpl.java
@@ -0,0 +1,177 @@
+package ru.practicum.service.comment;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.exception.BadRequestException;
+import ru.practicum.exception.DataConflictException;
+import ru.practicum.exception.DataNotFoundException;
+import ru.practicum.model.dto.comment.CommentDto;
+import ru.practicum.model.dto.comment.DeletedCommentDto;
+import ru.practicum.model.dto.comment.NewCommentDto;
+import ru.practicum.model.entity.CommentEntity;
+import ru.practicum.model.entity.EventEntity;
+import ru.practicum.model.entity.UserEntity;
+import ru.practicum.repostitory.CommentRepository;
+import ru.practicum.repostitory.EventRepository;
+import ru.practicum.repostitory.UserRepository;
+import ru.practicum.util.Pagenator;
+import ru.practicum.util.mapper.CommentMapper;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService {
+	private final CommentRepository commentRepository;
+	private final UserRepository userRepository;
+	private final EventRepository eventRepository;
+
+	@Override
+	@Transactional
+	public CommentDto updateCommentByAdmin(int commentId, NewCommentDto newCommentDto) throws DataNotFoundException {
+		CommentEntity commentToUpdate = commentRepository.findWithCommenterById(commentId).orElseThrow(
+				() -> new DataNotFoundException("Comment with id[" + commentId + " doesn't exist.")
+		);
+
+		setNewInfoForCommentByAdmin(commentToUpdate, newCommentDto);
+
+		return CommentMapper.entityToDto(commentRepository.save(commentToUpdate), commentToUpdate.getCommenter());
+	}
+
+	@Override
+	@Transactional
+	public DeletedCommentDto deleteCommentByAdmin(int commentId) throws DataNotFoundException {
+		CommentEntity commentToDelete = commentRepository.findById(commentId).orElseThrow(
+				() -> new DataNotFoundException("Comment with id[" + commentId + " doesn't exist.")
+		);
+
+		commentRepository.delete(commentToDelete);
+
+		return CommentMapper.entityToDeletedDto(commentToDelete);
+	}
+
+	@Override
+	@Transactional
+	public CommentDto getCommentByAdmin(int commentId) throws DataNotFoundException {
+		CommentEntity comment = commentRepository.findWithCommenterById(commentId).orElseThrow(
+				() -> new DataNotFoundException("Comment with id[" + commentId + " doesn't exist.")
+		);
+
+		return CommentMapper.entityToDto(comment, comment.getCommenter());
+	}
+
+	@Override
+	@Transactional
+	public List<CommentDto> getCommentsByAdmin(int eventId, String text, LocalDateTime start, LocalDateTime end,
+											   int from, int size) throws DataNotFoundException, BadRequestException {
+		if (start != null && end != null)
+			if (start.isAfter(end))
+				throw new BadRequestException("Range start cannot be after range end.");
+
+		eventRepository.findById(eventId).orElseThrow(
+				() -> new DataNotFoundException("Event with id[" + eventId + "] doesn't exist.")
+		);
+
+		List<CommentEntity> comments = commentRepository.findWithCommenterBySearchParam(eventId, text, start, end,
+				Pagenator.getPageable(from, size));
+
+		List<CommentDto> commentsDto = comments.stream()
+				.map(comment -> CommentMapper.entityToDto(comment, comment.getCommenter()))
+				.toList();
+
+		sortCommentsByCreatedOn(commentsDto);
+
+		return commentsDto;
+	}
+
+	@Override
+	@Transactional
+	public CommentDto addComment(int userId, int eventId, NewCommentDto newCommentDto) throws DataNotFoundException {
+		UserEntity commenter = userRepository.findById(userId).orElseThrow(
+				() -> new DataNotFoundException("User with id[" + userId + "doesn't exist.")
+		);
+
+		EventEntity event = eventRepository.findById(eventId).orElseThrow(
+				() -> new DataNotFoundException("Event with id[" + eventId + "doesn't exist.")
+		);
+
+		CommentEntity commentToSave = CommentMapper.newCommentDtoToEntity(commenter, event, newCommentDto);
+
+		return CommentMapper.entityToDto(commentRepository.save(commentToSave), commenter);
+	}
+
+	@Override
+	@Transactional
+	public DeletedCommentDto deleteCommentByUser(int userId, int commentId) throws DataNotFoundException,
+			DataConflictException {
+		UserEntity commenter = userRepository.findById(userId).orElseThrow(
+				() -> new DataNotFoundException("User with id[" + userId + "doesn't exist.")
+		);
+
+		CommentEntity commentToDelete = commentRepository.findWithCommenterById(commentId).orElseThrow(
+				() -> new DataNotFoundException("Comment with id[" + commentId + " doesn't exist.")
+		);
+
+		if (!commentToDelete.getCommenter().getId().equals(commenter.getId()))
+			throw new DataConflictException("User is not commenter.");
+
+		commentRepository.delete(commentToDelete);
+
+		return CommentMapper.entityToDeletedDto(commentToDelete);
+	}
+
+	@Override
+	@Transactional
+	public CommentDto getCommentByUser(int userId, int commentId) throws DataNotFoundException {
+		userRepository.findById(userId).orElseThrow(
+				() -> new DataNotFoundException("User with id[" + userId + "doesn't exist.")
+		);
+
+		CommentEntity comment = commentRepository.findWithCommenterById(commentId).orElseThrow(
+				() -> new DataNotFoundException("Comment with id[" + commentId + " doesn't exist.")
+		);
+
+		return CommentMapper.entityToDto(comment, comment.getCommenter());
+	}
+
+	@Override
+	@Transactional
+	public List<CommentDto> getCommentsByUser(int userId, int eventId, String text, LocalDateTime start,
+											  LocalDateTime end, int from, int size) throws DataNotFoundException, BadRequestException {
+		if (start != null && end != null)
+			if (start.isAfter(end))
+				throw new BadRequestException("Range start cannot be after range end.");
+
+		userRepository.findById(userId).orElseThrow(
+				() -> new DataNotFoundException("User with id [" + userId + "]  doesn't exist.")
+		);
+
+		eventRepository.findById(eventId).orElseThrow(
+				() -> new DataNotFoundException("Event with id[" + eventId + "] doesn't exist.")
+		);
+
+		List<CommentEntity> comments = commentRepository.findWithCommenterBySearchParam(eventId, text, start, end,
+				Pagenator.getPageable(from, size));
+
+		List<CommentDto> commentsDto = comments.stream()
+				.map(comment -> CommentMapper.entityToDto(comment, comment.getCommenter()))
+				.toList();
+
+		sortCommentsByCreatedOn(commentsDto);
+
+		return commentsDto;
+	}
+
+	private void setNewInfoForCommentByAdmin(CommentEntity commentToUpdate, NewCommentDto newCommentDto) {
+		commentToUpdate.setText(newCommentDto.getText());
+	}
+
+	private void sortCommentsByCreatedOn(List<CommentDto> comments) {
+		comments.sort(Comparator.comparing(CommentDto::getCreatedOn));
+	}
+}
